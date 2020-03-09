5a490c732eff1b217ddf6a1ba1109a318c270981
Dih5 <dihedralfive@gmail.com>
2020-03-09 14:12:20 +0100

    Add RTD support

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,26 @@
 import os
 import sys
 
-import pycbr
 sys.path.insert(0, os.path.abspath('..'))
+
+# Mock the needed packages on RTD
+# http://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+if os.environ.get('READTHEDOCS') == 'True':
+    from unittest.mock import MagicMock
+
+
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return MagicMock()
+
+
+    MOCK_MODULES = ["numpy", "pandas", "scikit-learn",
+                    "flask", "flask-restplus", "flask-cors"
+                    ]
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+import pycbr
 
 
 # -- General configuration ---------------------------------------------
--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@
 
 """The setup script"""
 
+import os
 from setuptools import setup, find_packages
+
+if os.environ.get('READTHEDOCS') == 'True':
+    requirements = []
+else:
+    requirements = ["numpy", "pandas", "scikit-learn",
+                    "flask", "flask-restplus", "flask-cors",
+                    ]
 
 setup(author="Dih5",
       author_email='dihedralfive@gmail.com',
       classifiers=[
           'Development Status :: 3 - Alpha',
-           # 'Intended Audience :: Science/Research',
+          'Intended Audience :: Science/Research',
           'Natural Language :: English',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.5',
@@ -19,15 +27,13 @@ setup(author="Dih5",
       ],
       description='Package to implement Case-Based Reasoning systems',
       extras_require={
-        "docs": ["nbsphinx", "sphinx-rtd-theme", "IPython"],
-        "test": ["pytest"],
+          "docs": ["nbsphinx", "sphinx-rtd-theme", "IPython"],
+          "test": ["pytest"],
       },
       keywords=[],
       name='pycbr',
       packages=find_packages(include=['pycbr'], exclude=["demos", "tests", "docs"]),
-      install_requires=["numpy", "pandas", "scikit-learn",
-                        "flask", "flask-restplus", "flask-cors"
-                        ],
+      install_requires=requirements,
       url='https://github.com/dih5/pycbr',
       version='0.1.0',
 
