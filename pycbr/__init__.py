429853c836a501331f32854f3020a5af7474c813
Dih5 <dihedralfive@gmail.com>
2023-10-19 16:47:28 +0200

    Bump version: 0.2.1 → 0.2.2

--- pycbr/__init__.py
+++ pycbr/__init__.py
@@ -1,6 +1,6 @@
 """pycbr - Package to implement Case-Based Reasoning systems"""
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Dih5 <dihedralfive@gmail.com>'
 
 from . import aggregate, models, recovery, casebase, server
